c4b84693b637f37dbcd4d4a1a459547063a6bdce
Ablai <ablainuraliev@gmail.com>
2019-04-14 20:19:10 +0600

    Week12

---
+++ Week12/todo-front/todo-front/src/app/shared/models/models.ts
@@ -0,0 +1,13 @@
+export interface ITaskList{
+  id:number;
+  name: string;
+}
+
+export interface ITask{
+  id:number;
+  _name:string;
+  c_at:Date;
+  d_on:Date;
+  status:String;
+  taskl:String;
+}
